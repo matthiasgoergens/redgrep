5a5f52f693baf5829ea2e5841b29e5da6e44e70f
Matthias Görgens <matthias.goergens@gmail.com>
2015-03-04 13:59:04 +0800

    Type stuff..

--- DFA2.hs
+++ DFA2.hs
@@ -52,35 +52,35 @@ data FMap x y where
     FMap :: FMap (x -> y) y
     deriving (Typeable)
 
-data Re a f x where
+data Re f x where
     -- Ranges of letters.  Nothing stands for .
     -- TODO: Add character classes later.
-    SymX :: Maybe [a] -> Re a f (f a)
+    SymX :: f -> Re f (Sym x)
     -- Alternative, |
-    AltX :: Re a f x -> Re a f y -> Re a f (Alt x y)
+    AltX :: Re f x -> Re f y -> Re f (Alt x y)
     -- Intersection
-    CutX :: Re a f x -> Re a f y -> Re a f (Cut x y)
+    CutX :: Re f x -> Re f y -> Re f (Cut x y)
     -- Sequencing
-    SeqX :: Re a f x -> Re a f y -> Re a f (Seq x y)
+    SeqX :: Re f x -> Re f y -> Re f (Seq x y)
     -- Repetition, Kleene Star *
-    RepX :: Re a f x -> Re a f (Rep x)
-    -- Plus :: Re a x -> Re a (NonEmptyList x)
+    RepX :: Re f x -> Re f (Rep x)
+    -- Plus :: Re x -> Re (NonEmptyList x)
     -- Complement
-    NotX :: Re a f x -> Re a f (Not x)
+    NotX :: Re f x -> Re f (Not x)
     -- Match empty string
-    EpsX :: x -> Re a f (Eps x)
+    EpsX :: x -> Re f (Eps x)
     -- Match no string
-    NilX :: Re a f Nil 
+    NilX :: Re f Nil 
     -- -- Do we need something like the lens trick?
     -- This might not work!
-    FMapX :: (x -> y) -> Re a f x -> Re a f (FMap x y)
+    FMapX :: (x -> y) -> Re f x -> Re f (FMap x y)
     deriving (Typeable)
 
-data BoolBefore a = BoolBefore Bool
-type ReBBefore a x = Re a BoolBefore x
+data BoolBefore = BoolBefore { before :: Bool }
+type ReBBefore x = Re BoolBefore x
 
-data BoolAfter a = BoolAfter Bool
-type ReBAfter a x = Re a BoolAfter x
+data BoolAfter = BoolAfter { after :: Bool }
+type ReBAfter x = Re BoolAfter x
 
 -- -- Funny enough, pass (later) works, but pass' doesn't type-check.
 -- -- even though we never really look at the first argument!
@@ -88,29 +88,39 @@ type ReBAfter a x = Re a BoolAfter x
 -- pass' a (CutT x' y') = CutT (pass' a x') (pass' a y')
 -- pass' a (SeqT x' y') = SeqT (pass' a x') (pass' a y')
 
--- 
--- -- This seems like boiler plate.
--- pass :: Ord a => Re a x -> a -> x -> x
--- pass (Sym x) a (SymT x') = SymT $ (pass' x a) x' where
---  pass' _ a False = False
---  pass' Nothing a True = True
---  pass' (Just l) a True = a `elem` l
--- 
--- pass (Alt x y) a (AltT x' y') = AltT (pass x a x') (pass y a y')
--- 
--- pass (Cut x y) a (CutT x' y') = CutT (pass x a x') (pass y a y')
--- 
--- pass (Seq x y) a (SeqT x' y') = SeqT (pass x a x') (pass y a y')
--- 
--- pass (Rep x) a (RepT x') = RepT $ pass x a x'
--- 
--- pass (Not x) a (NotT x') = NotT $ pass x a x'
--- 
--- pass (Eps x) a _ = EpsT
--- 
--- pass Nil a _ = NilT
--- 
--- -- pass (FMap _ x) a (FMapT x' y') = _ . FMapT  $ pass _ a _
+f :: Eq a => a -> Re BoolBefore x -> x -> Re BoolAfter x
+f a (SymX _) (Sym (Just l)) | _ a l = SymX (BoolAfter True)
+f a (AltX x' y') (Alt x y) = AltX (f a x' x) (f a y' y) -- AltX (f x x') (f y y')
+
+-- This seems like boiler plate.
+-- pass :: Eq a => x -> a -> Re BoolBefore x -> Re BoolAfter x
+-- pass (Sym _) a (SymX (BoolBefore False)) = SymX (BoolAfter False)
+-- pass (Sym Nothing) a (SymX (BoolBefore True)) = SymX . BoolAfter $ True
+ -- SymX . BoolAfter . _ $ pass' x a x' where
+{-    where
+ pass' _ _ _ = False
+ -- pass' _ a False = False
+ -- pass' Nothing a True = True
+ -- pass' (Just l) a True = a `elem` l
+-}
+
+{-
+pass (Alt x y) a (AltT x' y') = AltT (pass x a x') (pass y a y')
+
+pass (Cut x y) a (CutT x' y') = CutT (pass x a x') (pass y a y')
+
+pass (Seq x y) a (SeqT x' y') = SeqT (pass x a x') (pass y a y')
+
+pass (Rep x) a (RepT x') = RepT $ pass x a x'
+
+pass (Not x) a (NotT x') = NotT $ pass x a x'
+
+pass (Eps x) a _ = EpsT
+
+pass Nil a _ = NilT
+-}
+
+-- pass (FMap _ x) a (FMapT x' y') = _ . FMapT  $ pass _ a _
 -- 
 -- epsable :: Re a x -> Bool
 -- epsable (Sym _) = False
