087e69666f391bbe98ac9cb1c552deb52f4f6d75
Matthias Görgens <matthias.goergens@gmail.com>
2015-05-27 12:24:38 +0800

    shrink for REini

--- Shrink.hs
+++ Shrink.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
 module Shrink where
+import Prelude hiding (seq, not)
 
 import Final
 import Test.QuickCheck
+import Data.Bifunctor
 
 shrink' :: forall f s . REini f s -> [REini f s]
 shrink' (SymX r) = (SymX <$> shrink r) ++ [EpsX TooMany 'a', NilX TooMany]
@@ -27,3 +30,47 @@ shrink' (NotX x) =
 shrink' (EpsX f s) = [NilX f]
 shrink' (NilX f) = []
 shrink' (Bimap f s x) = Bimap f s <$> shrink' x
+
+data Shield r = forall f s . Shield (r f s)
+
+out :: Bifunctor r => Shield r -> r () ()
+out (Shield r) = bimap (const ()) (const ()) r
+
+
+altS, cutS :: RE r => Shield r -> Shield r -> Shield r
+altS (Shield x) (Shield y) = Shield (alt x y)
+cutS (Shield x) (Shield y) = Shield (cut x y)
+seqS (Shield x) (Shield y) = Shield (seq x y)
+
+repS (Shield x) = Shield (rep x)
+notS (Shield x) = Shield (not x)
+
+shrink2 op (Shield x) (Shield y)
+    = Shield nil_ : Shield eps_ : Shield x : Shield y
+    : tail (op <$> shrinkS' (Shield x) <*> shrinkS' (Shield y))
+shrink1 op (Shield x)
+    = Shield nil_ : Shield eps_
+    : (op <$> shrinkS (Shield x))
+
+shrinkS' :: Shield REini -> [Shield REini]
+shrinkS' x = x : shrinkS x
+
+shrinkS :: Shield REini -> [Shield REini]
+shrinkS (Shield (SymX r))
+    = Shield nil_
+    : Shield eps_
+    : (Shield . SymX <$> shrink r)
+shrinkS (Shield (AltX x y))
+    = shrink2 altS (Shield x) (Shield y)
+shrinkS (Shield (CutX x y))
+    = shrink2 cutS (Shield x) (Shield y)
+shrinkS (Shield (SeqX x y))
+    = shrink2 seqS (Shield x) (Shield y)
+shrinkS (Shield (RepX x))
+    = shrink1 repS (Shield x)
+shrinkS (Shield (NotX x))
+    = shrink1 notS (Shield x)
+shrinkS (Shield (EpsX f s))
+    = [Shield $ NilX f]
+shrinkS (Shield (NilX f))
+    = []
