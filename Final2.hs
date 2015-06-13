f27f35382fa7a1b06510ac3852580234a3c06dac
Matthias Görgens <matthias.goergens@gmail.com>
2015-06-13 09:23:51 +0800

    Clean up obsolete files

--- Final2.hs
+++
@@ -1,2 +0,0 @@
-class (CutI cut, AltI alt) => Alt r where
-    alt :: r f s -> r f' s' -> r (cut f f') (alt s s')
--- RedO.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module RedO where
-
-import Test.QuickCheck
-
-
-
-return []
-main = $quickCheckAll
